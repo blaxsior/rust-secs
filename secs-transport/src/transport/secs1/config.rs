1063e394268e7e6c14d0b22d52f6f4b8a44abc42
blaxsior <heejun2798@gmail.com>
2026-08-11 14:32:34 +0900

    feat: secs1 config serde 적용

--- secs-transport/src/transport/secs1/config.rs
+++ secs-transport/src/transport/secs1/config.rs
@@ -1,10 +1,11 @@
 use core::time::Duration;
+use serde::{Deserialize, Serialize};
 
 use crate::transport::{ConnectionRole, DeviceId};
 
 ///
 /// SECS-I 통신 구성 시 사용하는 설정
-///
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Secs1TransportConfig {
     /// 장치 식별자. 통신 장치의 식별 번호
     pub device_id: DeviceId,
@@ -13,12 +14,16 @@ pub struct Secs1TransportConfig {
     /// 트랜잭션 통신 구분을 위한 sourceId 값
     // pub source_id: u16,
     /// inter character timeout(ms): length byte ~ 2nd checksum byte
+    #[serde(with = "humantime_serde")]
     pub t1_timeout: Duration,
     /// protocol timeout(ms): ENQ ~ EOT / EOT ~ length / 2nd checksum ~ any char
+    #[serde(with = "humantime_serde")]
     pub t2_timeout: Duration,
     // send - recv timeout
+    #[serde(with = "humantime_serde")]
     pub t3_timeout: Duration,
     // inter block timeout
+    #[serde(with = "humantime_serde")]
     pub t4_timeout: Duration,
     pub t2_rty_limit: u8,
 }
